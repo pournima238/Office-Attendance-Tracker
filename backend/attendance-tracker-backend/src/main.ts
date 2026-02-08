4a59ba859245f5021026bcdad453d09d40406378
pournime238 <youremail@example.com>
2026-02-08 17:53:08 +0530

    Fix corrs issue for backend url

--- backend/attendance-tracker-backend/src/main.ts
+++ backend/attendance-tracker-backend/src/main.ts
@@ -1,10 +1,19 @@
+// backend/src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
-  console.log("Server started on port 3000")
+  app.enableCors({
+    origin: [
+      'http://localhost:4200', // For local development
+      'https://your-future-frontend-url.vercel.app' // Add your production URL later
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
